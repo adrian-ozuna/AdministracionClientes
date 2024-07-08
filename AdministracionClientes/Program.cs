7b0cda179910378bc2ce12eea15caf2ca6b53a70
Adrian Ozuna <adrian.gab.ozuna@outlook.com>
2024-07-08 12:15:52 -0400

    Added ApplicationDbContext and configured necessary services.

--- AdministracionClientes/Program.cs
+++ AdministracionClientes/Program.cs
@@ -1,4 +1,10 @@
+using AdministracionClientes.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
